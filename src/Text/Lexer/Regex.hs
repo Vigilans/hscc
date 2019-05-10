6f060775657fa8d2600a67d2559d71e47fb52fc1
Vigilans <vigilans@foxmail.com>
2019-05-10 10:50:28 +0800

    Replace regexFunction state with record matching

--- src/Text/Lexer/Regex.hs
+++ src/Text/Lexer/Regex.hs
@@ -70,51 +70,51 @@ data RegexFunction = RegexFunction {
 regexFunction :: Regex -> RegexFunction
 regexFunction re = execState (run re) (RegexFunction M.empty M.empty M.empty M.empty M.empty) where
     run n@Epsilon =
-        modify $ \s -> RegexFunction {
-            nullable  = M.insert n True $ nullable s,
-            firstpos  = M.insert n S.empty $ firstpos s,
-            lastpos   = M.insert n S.empty $ lastpos s,
-            followpos = followpos s,
-            leafsymb  = leafsymb s
+        modify $ \RegexFunction { nullable, firstpos, lastpos, followpos, leafsymb } -> RegexFunction {
+            nullable  = M.insert n True nullable,
+            firstpos  = M.insert n S.empty firstpos,
+            lastpos   = M.insert n S.empty lastpos,
+            followpos = followpos,
+            leafsymb  = leafsymb
         }
     run n@(Union c1 c2) = do
         run c1
         run c2
-        modify $ \s -> RegexFunction {
-            nullable  = M.insert n ((nullable s ! c1) || (nullable s ! c2)) $ nullable s,
-            firstpos  = M.insert n ((firstpos s ! c1) `S.union` (firstpos s ! c2)) $ firstpos s,
-            lastpos   = M.insert n ((lastpos  s ! c1) `S.union` (lastpos  s ! c2)) $ lastpos s,
-            followpos = followpos s,
-            leafsymb  = leafsymb s
+        modify $ \RegexFunction { nullable, firstpos, lastpos, followpos, leafsymb } -> RegexFunction {
+            nullable  = M.insert n ((nullable ! c1) || (nullable ! c2)) nullable,
+            firstpos  = M.insert n ((firstpos ! c1) `S.union` (firstpos ! c2)) firstpos,
+            lastpos   = M.insert n ((lastpos  ! c1) `S.union` (lastpos  ! c2)) lastpos,
+            followpos = followpos,
+            leafsymb  = leafsymb
         }
     run n@(Concat c1 c2) = do
         run c1
         run c2
-        modify $ \s -> RegexFunction {
-            nullable  = M.insert n ((nullable s ! c1) && (nullable s ! c2)) $ nullable s,
-            firstpos  = M.insert n ((firstpos s ! c1) `S.union` (if nullable s ! c1 then firstpos s ! c2 else S.empty)) $ firstpos s,
-            lastpos   = M.insert n ((lastpos  s ! c2) `S.union` (if nullable s ! c2 then lastpos  s ! c1 else S.empty)) $ lastpos s,
-            followpos = S.foldr (M.adjust $ S.union (firstpos s ! c2)) (followpos s) (lastpos s ! c1),
-            leafsymb  = leafsymb s
+        modify $ \RegexFunction { nullable, firstpos, lastpos, followpos, leafsymb } -> RegexFunction {
+            nullable  = M.insert n ((nullable ! c1) && (nullable ! c2)) nullable,
+            firstpos  = M.insert n ((firstpos ! c1) `S.union` (if nullable ! c1 then firstpos ! c2 else S.empty)) firstpos,
+            lastpos   = M.insert n ((lastpos  ! c2) `S.union` (if nullable ! c2 then lastpos  ! c1 else S.empty)) lastpos,
+            followpos = S.foldr (M.adjust $ S.union (firstpos ! c2)) followpos (lastpos ! c1),
+            leafsymb  = leafsymb
         }
     run n@(Closure c) = do
         run c
-        modify $ \s -> RegexFunction {
-            nullable  = M.insert n True $ nullable s,
-            firstpos  = M.insert n (firstpos s ! c) $ firstpos s,
-            lastpos   = M.insert n (lastpos  s ! c) $ lastpos s,
-            followpos = S.foldr (M.adjust $ S.union (firstpos s ! n)) (followpos s) (lastpos s ! n),
-            leafsymb  = leafsymb s
+        modify $ \RegexFunction { nullable, firstpos, lastpos, followpos, leafsymb } -> RegexFunction {
+            nullable  = M.insert n True nullable,
+            firstpos  = M.insert n (firstpos ! c) firstpos,
+            lastpos   = M.insert n (lastpos  ! c) lastpos,
+            followpos = S.foldr (M.adjust $ S.union (firstpos ! n)) followpos (lastpos ! n),
+            leafsymb  = leafsymb
         }
     run n = do -- Symbol or Endmarker
         poses <- gets leafsymb
         let i = maximum (0 : M.keys poses) + 1
-        modify $ \s -> RegexFunction {
-            nullable  = M.insert n False $ nullable s,
-            firstpos  = M.insert n (S.singleton i) $ firstpos s,
-            lastpos   = M.insert n (S.singleton i) $ lastpos s,
-            followpos = M.insert i S.empty $ followpos s,
-            leafsymb  = M.insert i n $ leafsymb s
+        modify $ \RegexFunction { nullable, firstpos, lastpos, followpos, leafsymb } -> RegexFunction {
+            nullable  = M.insert n False nullable,
+            firstpos  = M.insert n (S.singleton i) firstpos,
+            lastpos   = M.insert n (S.singleton i) lastpos,
+            followpos = M.insert i S.empty followpos,
+            leafsymb  = M.insert i n leafsymb
         }
 
 data Regex2DFAState = R2DS {
