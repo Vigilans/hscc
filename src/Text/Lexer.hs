627fbf00564c0e9306cdaf3009cce21614930b60
Vigilans <vigilans@foxmail.com>
2019-05-15 19:00:05 +0800

    Finish lexer file parsing

--- src/Text/Lexer.hs
+++ src/Text/Lexer.hs
@@ -4,62 +4,72 @@ module Text.Lexer (
 
 import qualified Data.Map as M
 import Text.ParserCombinators.Parsec
+import Text.Parsec.Char
+import Text.Lexer.DFA
+import Text.Lexer.DFA.Regex
 import Text.Lexer.Regex
-import Debug.Trace
-import Data.List
-import qualified Text.Regex.Posix as R
+import Control.Monad
+
 type Action = String
 
 data Lexer = Lexer {
-    definitionSection :: String,
-    regexDefinitions  :: M.Map String Regex,  -- Tag to Regex
-    regexActions      :: M.Map String Action, -- Tag to Action
-    userSubroutines   :: String
-} deriving (Show)
-
-data LexFile = LexFile {
-    headSection         :: String,
-    regularDefininions  :: M.Map String String, -- def to RE
-    regularExpressions  :: M.Map String String, -- RE to Action
-    tailSection         :: String
+    userDefs :: String,
+    regexDef :: M.Map String Regex,  -- RE name to Regex
+    regexAct :: M.Map String Action, -- RE literal to Action
+    regexDFA :: DFA,
+    userCode :: String
 } deriving (Show)
 
 parseLexer :: GenParser Char Lexer Lexer
 parseLexer = do
-    r <- parseRegex
-    updateState $ \l@Lexer { regexDefinitions } -> l {
-        regexDefinitions = M.insert "Hello, World" r regexDefinitions
-    }
+    skipMany trim
+    -- Parse Definition Section
+    userDefs <- betweenStr "%{" "%}" anyChar
+    updateState $ \lexer -> lexer { userDefs }
+    -- Parse Regex Definitions
+    manyTill (trim <|> parseRegexDef) (string "%%")
+    -- Parse Regex & Actions
+    manyTill (trim <|> parseRegexAct) (string "%%")
+    -- Parse User Code
+    userCode <- manyTill anyChar eof
+    updateState $ \lexer -> lexer { userCode }
+    -- Return final state
     getState
+
+parseRegexDef :: GenParser Char Lexer ()
+parseRegexDef = do
+    Lexer { regexDef } <- getState
+    name  <- manyTill anyChar space
+    _     <- trim
+    regex <- parseRegex regexDef
+    updateState $ \lexer -> lexer {
+        regexDef = M.insert name regex regexDef
+    }
+
+parseRegexAct :: GenParser Char Lexer ()
+parseRegexAct = do
+    Lexer { regexDef } <- getState
+    literal <- lookAhead $ manyTill anyChar space
+    regex   <- parseRegex regexDef
+    _       <- trim
+    action  <- manyTill anyChar space
+    updateState $ \lexer@Lexer { regexAct, regexDFA } -> lexer {
+        regexAct = M.insert literal action regexAct,
+        regexDFA = union regexDFA $ regex2dfa regex literal -- Use literal as tag
+    }
+
+trim :: GenParser Char Lexer ()
+trim = void space <|> void comment -- Trim spaces and comments
+
+comment :: GenParser Char Lexer String
+comment = betweenStr "/*" "*/" anyChar
+
+betweenStr :: String -> String -> GenParser Char st a -> GenParser Char st [a]
+betweenStr open close p = string open >> manyTill p (string close)
 
 input = "a|b|c"
 
 lexFileInput = "  \n\t\n%{\n\n#include <stdio.h>\naaa\n%}\nD\t[0-9]\nA\t[1-3]*\n%%\nauto\t{ printf(\"AUTO\"); }\ncase\t{ printf(\"CASE\"); }\n{D}\t{ printf(\"IDENTIFIER\"); }\n%%\nmain() {\tyylex();\n}"
-
-lexFile :: GenParser Char st [String]
-lexFile = do
-    many (oneOf "\n\t\f\v ")
-    string "%{\n"
-    headSec <- manyTill anyChar (try (string "\n%}\n"))
-    
-    rdSec <- manyTill anyChar (try (string "\n%%\n"))
-    reSec <- manyTill anyChar (try (string "\n%%\n"))
-    usrSec <- many anyChar
-    return [headSec, rdSec, reSec, usrSec]
-
-
--- buildRD :: String -> M.Map String String
--- buildRD s = foldl handle M.empty (lines s) where
---     handle acc l = M.insert def pat acc where
---         -- def = head (head ((strip l) R.=~"[^\t\f\v ]*"::[[String]]))
---         def = "bb"
---         pat = "aa"
-
-
-
-
-parseLex :: String -> Either ParseError [String]
-parseLex input = parse lexFile "err" input
 
 stripL :: String -> String -> String
 stripL x = dropWhile (`elem` x)
@@ -69,6 +79,6 @@ stripR x = reverse . stripL x . reverse
 
 strip :: String -> String -> String
 strip x = stripL x . stripR x
-run = case runParser parseLexer (Lexer "blabla" M.empty M.empty "lablab") "sdfas" input of
-    Left err -> error $ show err
-    Right lexer -> lexer
+-- run = case runParser parseLexer (Lexer "blabla" M.empty M.empty "lablab") "sdfas" input of
+--     Left err -> error $ show err
+--     Right lexer -> lexer
