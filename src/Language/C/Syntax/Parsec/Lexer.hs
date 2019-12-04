4c71298d0cad10f6eed496aeeb19f923b18c986d
Vigilans <vigilans@foxmail.com>
2019-12-04 17:19:08 +0800

    Add Language C's constant and type lexing

--- src/Language/C/Syntax/Parsec/Lexer.hs
+++ src/Language/C/Syntax/Parsec/Lexer.hs
@@ -35,3 +35,35 @@ minicDef = emptyDef {
 
 minicLexer :: Parsec.TokenParser st
 minicLexer@TokenParser{..} = Parsec.makeTokenParser minicDef
+
+-- Constant
+
+integerConstant :: GenParser Char st (Typed Lit)
+integerConstant = (Integer Int Signed, ) . IntegerLit <$> integer
+
+characterConstant :: GenParser Char st (Typed Lit)
+characterConstant = (Integer Char Signed, ) . CharLit <$> charLiteral
+
+floatingConstant :: GenParser Char st (Typed Lit)
+floatingConstant = (Floating Double, ) . FloatLit <$> float
+
+stringConstant :: GenParser Char st (Typed Lit)
+stringConstant = (stringType &&& StringLit) <$> stringLiteral
+
+-- Type
+
+primitiveType :: GenParser Char st TypeInfo
+primitiveType = (reserved "void"   $> Right Void)
+            <|> (reserved "char"   $> Right (Integer Char Unsigned))
+            <|> (reserved "short"  $> Right (Integer Short Signed))
+            <|> (reserved "int"    $> Right (Integer Int Signed))
+            <|> (reserved "long"   $> Right (Integer Long Signed))
+            <|> (reserved "float"  $> Right (Floating Float))
+            <|> (reserved "double" $> Right (Floating Double))
+            <|> (reserved "signed"   $> Left (turnSignedness Signed))
+            <|> (reserved "unsigned" $> Left (turnSignedness Unsigned))
+
+turnSignedness :: Signedness -> Type -> Type
+turnSignedness s Unknown = Integer Int s
+turnSignedness s (Integer t _) = Integer t s
+turnSignedness s _ = error "Type is not of integer type"
